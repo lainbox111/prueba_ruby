5342138a934dbc7d8d3b5fae00cdafe82cdd99d9
Esteban Pena <estebanpena111@protonmail.com>
2019-05-14 14:31:40 +0300

    final

--- prueba.rb
+++ prueba.rb
@@ -1,3 +1,4 @@
+
 def read_alum(file_name)
 file = File.open(file_name, 'r')
 alum = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
@@ -5,39 +6,84 @@ file.close
 alum
 end
 
-
-print read_alum("alumnos.csv")
-
+alumn_list = read_alum("alumnos.csv")
 
 
+def promedio
+  arreglo = read_alum("alumnos.csv")
+  sum = 0.0
+  arreglo.each do |el|
+  nombre_alumno = el.shift
+  el.collect! {|i| i.to_f}
+    sum = el.inject(:+)
+   puts "#{nombre_alumno} tiene un promedio de #{sum / el.size}"
+ end
+end
 
 
+def inasistencias
+falta = 0
+arreglo = read_alum("alumnos.csv")
+
+  arreglo.each do |el|
+  nombre_alumno = el.shift
+  el.each do |i|
+    if falta != 0
+      falta = 0
+    else i == "A"
+      falta = falta + 1
+    end
+  end
+puts "#{nombre_alumno} tiene #{falta} inasistencias"
+end
+end
 
 
+def aprovados
+  arreglo = read_alum("alumnos.csv")
+  sum = 0.0
+  requisito = 5.0
+
+  arreglo.each do |el|
+  nombre_alumno = el.shift
+  el.collect! {|i| i.to_f}
+    sum = el.inject(:+)
+    prom = sum / el.size
+
+    if prom >= requisito
+      puts "#{nombre_alumno} aprovó el curso"
+    end
+ end
+end
 
 
-
-
-
-#MENU á, é, í, ó, ú
 
 condition = 0
 while condition != 4
-  puts "Elije una opción entre 1 y 3. Para salir, elije 4"
-  puts "#OPCION 1: "
-  puts "#OPCION 2: "
-  puts "#OPCION 3: "
+  puts ""
+  puts "Elije una opción entre 1 y 3. Para salir, elije 4 "
+  puts ""
+  puts "#OPCION 1: Promedio cada alumno"
+  puts "#OPCION 2: Cantidad de inasistencias"
+  puts "#OPCION 3: Alumnos aprovados"
   condition = gets.chomp.to_i
   case condition
   when 1
-
+  puts ""
+    promedio
+  puts ""
   when 2
-    sum_stock
+  puts ""
+    inasistencias
+  puts ""
   when 3
-    max_stock
+  puts ""
+    aprovados
+  puts ""
   when 4
-    min_stock
+  puts ""
     puts "Hasta luego !"
+  puts ""
   else
     puts "porfavor ingrese una opción válida"
   end
